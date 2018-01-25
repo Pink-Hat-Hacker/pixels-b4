54ec23e23a7b852838d2654c5dc9b8a54eb933f3
Zoe Valladares <zvalladares@wlapcs.org>
2018-01-25 20:27:22 +0000

    first

--- sketch.js
+++ sketch.js
@@ -1,8 +1,21 @@
 function setup(){
     createCanvas(600,400);
-    background(0);
 }
 
 function draw(){
+    background(0);
     
+    loadPixels();
+    
+    for(var row = 0; row < height; row++){
+        for(var col = 0; col < width; col++){
+            var startingIndex = (col + row * width)*4
+            pixels[startingIndex] = col; //red 
+            pixels[startingIndex + 1] = row-33; //green
+            pixels[startingIndex + 2] = 234; //blue
+            pixels[startingIndex + 3] = 130; //alpha
+        }
+    }
+    
+    updatePixels();
 }
